10673cd322a76cc3acde27d57c818fc5ecb2312c
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-12-06 22:20:37 +0100

    Added vuex persistence

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 import AuthModule from '@/store/modules/auth'
 
@@ -7,9 +8,15 @@ import { StoreState } from '@/store/types'
 
 Vue.use(Vuex)
 
+const vuexLocal = new VuexPersistence<StoreState>({
+    modules: [ 'auth' ],
+    storage: window.localStorage
+})
+
 export default new Vuex.Store<StoreState>({
     modules: {
         auth: AuthModule
     },
+    plugins: [ vuexLocal.plugin ],
     strict: process.env.NODE_ENV === 'development'
 })
