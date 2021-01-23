c1d40ad97583f3224b47b1da12711410b4cdb175
filip <filip.raszka3@gmail.com>
2021-01-23 20:37:28 +0100

    implement submit test

--- discite_app/views.py
+++ discite_app/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from discite_app.services import show_test_service, create_test_service
+from discite_app.services import show_test_service, create_test_service, submit_test_service
 
 jwt = JWTAuthentication()
 
@@ -29,4 +29,18 @@ def create_test(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         create_test_service.create_test(data, current_user)
+        return HttpResponse('')
+
+
+@csrf_exempt
+def submit_test(request, test_id):
+    try:
+        auth_tuple = jwt.authenticate(request)
+        current_user = auth_tuple[0]
+    except:
+        return HttpResponse('Unathorized', status=401)
+
+    if request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        submit_test_service.submit_test(data, current_user, test_id)
         return HttpResponse('')
